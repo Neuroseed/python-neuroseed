0ed1ce72893719079ca1d44dcfd151782b036031
Ihor Omelchenko <counter3d@gmail.com>
2018-03-24 23:58:57 +0200

    Add tests base

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
+import os
+import codecs
+import re
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+requires = [
+        'requests==2.18.1',
+        'jsonschema==2.6.0'
+]
+
+tests_require = []
 
 setup(
     name='neuroseed',
-    version='0.2.0',
+    version=find_version('neuroseed/__init__.py'),
     description='Neuroseed platform REST API wrapper',
     author='Ihor Omelchenko',
     author_email='counter3d@gmail.com',
@@ -13,9 +39,9 @@ setup(
         'Programming Language :: Python :: 3'
     ],
     packages=find_packages(exclude=['examples', 'docs', 'tests']),
-    install_requires=[
-        'requests==2.18.1',
-        'jsonschema==2.6.0'
-    ]
+    install_requires=requires,
+    extras_require={
+        'testing': tests_require,
+    },
+    test_suite='tests'
 )
-
